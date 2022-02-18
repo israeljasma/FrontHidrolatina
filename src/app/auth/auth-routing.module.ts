0c4ff448a07c73674a7470a927cc9db6008488b2
Israel Jasma <by.darklancer@gmail.com>
2022-02-18 14:16:45 -0300

    Feat: Rutas hijas de Auth component declaradas (login)

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './pages/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+    ]
+  }
+]
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild( routes )
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AuthRoutingModule { }
