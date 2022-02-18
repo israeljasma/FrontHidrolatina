55b32a4855f15a23be91079a35f7c025e1c767e9
Israel Jasma <by.darklancer@gmail.com>
2022-02-18 14:17:35 -0300

    Feat: AuthRouting declarado en auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { LoginComponent } from './pages/login/login.component';
+
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 
@@ -9,7 +12,8 @@ import { LoginComponent } from './pages/login/login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
