0c450732e600312f266aae421aa67bdb5de217e6
Israel Jasma <by.darklancer@gmail.com>
2022-02-23 16:43:49 -0300

    Feat: Pantalla de carga para los detalles de usuarios

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -14,8 +16,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   ],
   exports: [
     MatButtonModule,
+    MatGridListModule,
     MatListModule,
     MatIconModule,
+    MatProgressSpinnerModule,
     MatSidenavModule,
     MatToolbarModule
   ]
