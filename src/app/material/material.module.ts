683c20fc900a39a3e462001b2525a70278655605
Israel Jasma <by.darklancer@gmail.com>
2022-02-24 15:32:33 -0300

    Feat: Material components Mat input, Mat Form Field declarados

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -17,8 +19,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports: [
     MatButtonModule,
     MatGridListModule,
-    MatListModule,
     MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatFormFieldModule,
     MatProgressSpinnerModule,
     MatSidenavModule,
     MatToolbarModule
