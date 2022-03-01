b21558df150cb0bc2a69311896b3713bd90a1f6e
Israel Jasma <by.darklancer@gmail.com>
2022-03-01 17:27:36 -0300

    Feat: cambios en interface user

--- src/app/user/interface/user.interface.ts
+++ src/app/user/interface/user.interface.ts
@@ -1,15 +1,15 @@
 export interface User {
-    id:               number;
+    id?:               number;
     password:         string;
-    last_login:       Date;
-    is_superuser:     boolean;
+    last_login?:       Date;
+    is_superuser?:     boolean;
     username:         string;
     email:            string;
     name:             string;
     last_name:        string;
-    is_active:        boolean;
-    is_staff:         boolean;
-    nfc:              number | null;
-    groups:           any[];
-    user_permissions: any[];
+    is_active?:        boolean;
+    is_staff?:         boolean;
+    nfc?:              number | null;
+    groups?:           any[];
+    user_permissions?: any[];
 }
