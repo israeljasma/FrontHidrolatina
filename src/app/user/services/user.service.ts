1181d05717c685219c9ac56dc5c3f6bc7778aecc
Israel Jasma <by.darklancer@gmail.com>
2022-03-01 21:38:06 -0300

    Feat: endpoint actualizar usuario

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -40,4 +40,11 @@ export class UserService {
 
     return this.http.post<User>(url, user);
   }
+
+  updateUser( user: User ): Observable<User>{
+
+    const url = `${ this.apiEndpoint }/users/${ user.id }/`;
+
+    return this.http.put<User>(url, user);
+  }
 }
