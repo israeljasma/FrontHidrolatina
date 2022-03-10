e40bc161743e8d3027222e79324b1fe87c53ecc5
Israel Jasma <by.darklancer@gmail.com>
2022-03-10 16:51:11 -0300

    Feat: module ppe

---
+++ src/app/dashboard-admin/ppes/ppes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PpesRoutingModule } from './ppes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PpesRoutingModule
+  ]
+})
+export class PpesModule { }
