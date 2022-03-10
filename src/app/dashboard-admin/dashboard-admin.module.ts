10296c7ae0f89efd49f3ee5f4ef7dca8363bdd9e
Israel Jasma <by.darklancer@gmail.com>
2022-03-10 06:05:26 -0300

    Feat: Component Dashboard

--- src/app/dashboard-admin/dashboard-admin.module.ts
+++ src/app/dashboard-admin/dashboard-admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardAdminRoutingModule } from './dashboard-admin-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
     DashboardAdminRoutingModule
