0f5133d219498ba4995747d90c18b76df544594e
Israel Jasma <by.darklancer@gmail.com>
2022-03-10 12:17:26 -0300

    Feat: Routing dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,10 @@ const routes: Routes = [
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
   },
   {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard-admin/dashboard-admin.module').then( m => m.DashboardAdminModule )
+  },
+  {
     path: 'users',
     loadChildren: () => import('./user/user.module').then( m => m.UserModule ),
     canLoad: [AuthGuard],
