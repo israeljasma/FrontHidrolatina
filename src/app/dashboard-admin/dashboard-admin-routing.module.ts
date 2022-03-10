1fa44e4c8444010c4ee36115ee45ca80a745d697
Israel Jasma <by.darklancer@gmail.com>
2022-03-10 12:45:27 -0300

    Feat: routing home component

--- src/app/dashboard-admin/dashboard-admin-routing.module.ts
+++ src/app/dashboard-admin/dashboard-admin-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent
+    component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
   }
 ];
 
