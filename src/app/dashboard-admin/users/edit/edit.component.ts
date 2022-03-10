4547cd7e5044d0bf50c15254bfc42cb021362c7e
Israel Jasma <by.darklancer@gmail.com>
2022-03-10 15:36:33 -0300

    Feat: Edit user implemented

--- src/app/dashboard-admin/users/edit/edit.component.ts
+++ src/app/dashboard-admin/users/edit/edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/user/services/user.service';
+import { User } from '../interfaces/users.intarface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { switchMap } from 'rxjs';
+import { ConfirmComponent } from '../components/confirm/confirm.component';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +14,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  user: User = {
+    username: '',
+    name: '',
+    last_name: '',
+    password: '',
+    email: ''
   }
 
+  constructor( private usersService: UserService, private activatedRoute: ActivatedRoute, private router: Router, private snackBar: MatSnackBar, private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.pipe( switchMap( ({ id }) => this.usersService.getUserDetail(id)) ).subscribe( user => this.user = user );
+  }
+
+  updateUser() {
+    if( this.user.username.trim().length === 0 ) {
+      return;
+    }else if( this.user.name.trim().length === 0 ) {
+      return;
+    }else if( this.user.last_name.trim().length === 0 ) {
+      return;
+    }else if( this.user.email.trim().length === 0 ) {
+      return;
+    }
+
+    this.usersService.updateUser( this.user ).subscribe( user => { console.log('Actualizando', user ), this.showSnackBar('Usuario actualizado con exito!') });
+  }
+
+  delete(){
+    const dialog = this.dialog.open( ConfirmComponent, {
+      width: '300px',
+      data: this.user
+    } );
+
+    dialog.afterClosed().subscribe( (result ) => {
+      if (result) {
+        this.usersService.deleteUser( this.user.id!.toString() ).subscribe( resp => { this.router.navigate(['/users']), this.showSnackBar('Usuario eliminado con exito!') });
+      }
+    } );
+  }
+
+  showSnackBar(message: string){
+    this.snackBar.open(message, 'Cerrar', { duration:3500 })
+  }
 }
--- src/app/dashboard-admin/users/users-routing.module.ts
+++ src/app/dashboard-admin/users/users-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './create/create.component';
 import { DetailComponent } from './detail/detail.component';
 import { ListComponent } from './list/list.component';
+import { EditComponent } from './edit/edit.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'create',
     component: CreateComponent
+  },
+  {
+    path: 'edit/:id',
+    component: EditComponent
   },
   {
     path: ':id',
