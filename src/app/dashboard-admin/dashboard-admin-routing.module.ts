fd25bfd1e59f1ef6f8c245d83db41da300f36784
Israel Jasma <by.darklancer@gmail.com>
2022-03-10 13:52:55 -0300

    Feat: Routing user list navbar

--- src/app/dashboard-admin/dashboard-admin-routing.module.ts
+++ src/app/dashboard-admin/dashboard-admin-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
       {
         path: '',
         component: HomeComponent
+      },
+      {
+        path: 'users',
+        loadChildren: () => import('./users/users.module').then( m => m.UsersModule )
       }
     ]
   }
