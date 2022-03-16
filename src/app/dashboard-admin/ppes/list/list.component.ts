01050a11e1b679d4b8314a4d630d1a8874837d56
Israel Jasma <israel.jasma@gmail.com>
2022-03-16 17:28:13 -0300

    Feat: tablas listado ppe

--- src/app/dashboard-admin/ppes/list/list.component.ts
+++ src/app/dashboard-admin/ppes/list/list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Ppe } from '../interfaces/ppes.intarface';
+import { PpesService } from '../services/ppes.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['username', 'name', 'last_name', 'helmet', 'headphones', 'goggles', 'gloves', 'boots', 'date'];
+  dataSource = new MatTableDataSource<Ppe>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  
+  constructor( private ppesService: PpesService ) { }
 
   ngOnInit(): void {
+    this.ppesService.getPpeList().subscribe( dataSource => this.dataSource.data = dataSource );
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
