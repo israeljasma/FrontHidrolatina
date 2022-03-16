bc0b90e8e51c7f4714eee5a35bdd085425dac4e6
Israel Jasma <israel.jasma@gmail.com>
2022-03-16 17:25:08 -0300

    Feat: servicio get para ppe

--- src/app/dashboard-admin/ppes/services/ppes.service.ts
+++ src/app/dashboard-admin/ppes/services/ppes.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Ppe } from '../interfaces/ppes.intarface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PpesService {
 
-  constructor() { }
+  private apiEndpoint: string = environment.apiEndpoint;
+
+  constructor( private http: HttpClient ) { }
+
+  public results: Ppe[] = [];
+
+  getPpeList(): Observable <any> {
+
+    const url = `${ this.apiEndpoint }/ppes/ppe/`;
+
+    this.http.get<Ppe[]>(url).subscribe( (resp) => {
+      console.log( resp );
+      this.results = resp;
+    });
+    this.results
+    return this.http.get(url);
+  }
 }
