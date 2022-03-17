62227e989f2e6d0609737bb8a2cf7e6ebfcedaf2
Israel Jasma <israel.jasma@gmail.com>
2022-03-16 22:16:01 -0300

    Feat: Servicio getList ActionsDetections

--- src/app/dashboard-admin/actions/services/actions.service.ts
+++ src/app/dashboard-admin/actions/services/actions.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { ActionDetection } from '../interfaces/actions.intarface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActionsService {
 
-  constructor() { }
+  private apiEndpoint: string = environment.apiEndpoint;
+
+  constructor( private http: HttpClient ) { }
+
+  public results: ActionDetection[] = [];
+
+  getActionDetectionList(): Observable<any> {
+
+    const url = `${ this.apiEndpoint }/actiondetections/action/`;
+
+    this.http.get<ActionDetection[]>(url).subscribe( resp => this.results = resp );
+
+    return this.http.get(url);
+  }
 }
