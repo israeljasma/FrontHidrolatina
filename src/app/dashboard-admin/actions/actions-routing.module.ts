43f20fbbfd67f380a751102270398547fda2c89c
Israel Jasma <israel.jasma@gmail.com>
2022-03-17 18:29:21 -0300

    Feat: routing list by user into action

--- src/app/dashboard-admin/actions/actions-routing.module.ts
+++ src/app/dashboard-admin/actions/actions-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListByUserComponent } from './list-by-user/list-by-user.component';
 import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ListComponent
+  },
+  {
+    path: ':id',
+    component: ListByUserComponent
   }
 ];
 
