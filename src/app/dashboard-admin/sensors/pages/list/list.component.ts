929609c7f37267573dfd953e107ae532fd18e836
Israel Jasma <israel.jasma@gmail.com>
2022-04-26 17:28:23 -0400

    Feat: Graficos sensores

--- src/app/dashboard-admin/sensors/pages/list/list.component.ts
+++ src/app/dashboard-admin/sensors/pages/list/list.component.ts
@@ -1,4 +1,7 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
+import { SensorsService } from '../../services/sensors.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +10,356 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  public feedConductivityData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public conductivityPermeateMembranesData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public conductivityPermeateVesselsData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public feedPressureVesselsData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public feedTemperatureData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public membraneFeedPressureData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public membranePermeateData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public membraneRejectionFlowData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public membraneRejectionPressureData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public rejectPressureVesselsData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public vesselsFeedingFlowData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  public vesselsPermeateFlowData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [],
+        label: 'Series A',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        fill: 'origin',
+      }
+    ],
+    labels: []
+  };
+
+  constructor( private sensorsService: SensorsService, private datePipe: DatePipe ) { }
 
   ngOnInit(): void {
+    this.getDataFeedConductivity();
+    this.getDataConductivityPermeatemembranes();
+    this.getDataConductivityPermeateVessels();
+    this.getDataFeedPressureVessels();
+    this.getDataFeedTemperature();
+    this.getDataMembraneFeedPressure();
+    this.getDataMembranePermeate();
+    this.getDataMembraneRejectionFlow();
+    this.getDataMembraneRejectionPressure()
+    this.getDataRejectPressureVessels()
+    this.getDataVesselsFeedingFlow()
+    this.getDataVesselsPermeateFlow()
   }
 
+  getDataFeedConductivity(){
+    this.sensorsService.getFeedConductivity().subscribe(
+      result => {
+        for (let x in result) {
+          this.feedConductivityData.datasets[0].data.push(result[x].number)
+          this.feedConductivityData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
+
+  getDataConductivityPermeatemembranes(){
+    this.sensorsService.getConductivityPermeatemembranes().subscribe(
+      result => {
+        for (let x in result) {
+          this.conductivityPermeateMembranesData.datasets[0].data.push(result[x].number)
+          this.conductivityPermeateMembranesData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
+
+  getDataConductivityPermeateVessels(){
+    this.sensorsService.getConductivityPermeateVessels().subscribe(
+      result => {
+        for (let x in result) {
+          this.conductivityPermeateVesselsData.datasets[0].data.push(result[x].number)
+          this.conductivityPermeateVesselsData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
+
+  getDataFeedPressureVessels(){
+    this.sensorsService.getFeedPressureVessels().subscribe(
+      result => {
+        for (let x in result) {
+          this.feedPressureVesselsData.datasets[0].data.push(result[x].number)
+          this.feedPressureVesselsData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
+
+  getDataFeedTemperature(){
+    this.sensorsService.getFeedTemperature().subscribe(
+      result => {
+        for (let x in result) {
+          this.feedTemperatureData.datasets[0].data.push(result[x].number)
+          this.feedTemperatureData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
+
+  getDataMembraneFeedPressure(){
+    this.sensorsService.getMembraneFeedPressure().subscribe(
+      result => {
+        for (let x in result) {
+          this.membraneFeedPressureData.datasets[0].data.push(result[x].number)
+          this.membraneFeedPressureData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
+
+  getDataMembranePermeate(){
+    this.sensorsService.getMembranePermeate().subscribe(
+      result => {
+        for (let x in result) {
+          this.membranePermeateData.datasets[0].data.push(result[x].number)
+          this.membranePermeateData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
+
+  getDataMembraneRejectionFlow(){
+    this.sensorsService.getMembraneRejectionFlow().subscribe(
+      result => {
+        for (let x in result) {
+          this.membraneRejectionFlowData.datasets[0].data.push(result[x].number)
+          this.membraneRejectionFlowData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
+
+  getDataMembraneRejectionPressure(){
+    this.sensorsService.getMembraneRejectionPressure().subscribe(
+      result => {
+        for (let x in result) {
+          this.membraneRejectionPressureData.datasets[0].data.push(result[x].number)
+          this.membraneRejectionPressureData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
+
+  getDataRejectPressureVessels(){
+    this.sensorsService.getRejectPressureVessels().subscribe(
+      result => {
+        for (let x in result) {
+          this.rejectPressureVesselsData.datasets[0].data.push(result[x].number)
+          this.rejectPressureVesselsData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
+
+  getDataVesselsFeedingFlow(){
+    this.sensorsService.getVesselsFeedingFlow().subscribe(
+      result => {
+        for (let x in result) {
+          this.vesselsFeedingFlowData.datasets[0].data.push(result[x].number)
+          this.vesselsFeedingFlowData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
+
+  getDataVesselsPermeateFlow(){
+    this.sensorsService.getVesselsPermeateFlow().subscribe(
+      result => {
+        for (let x in result) {
+          this.vesselsPermeateFlowData.datasets[0].data.push(result[x].number)
+          this.vesselsPermeateFlowData.labels?.push(this.datePipe.transform(result[x].time, 'yyyy-MM-dd'))
+        }
+      }
+    )
+  }
 }
