1068b69263dbcd7a7398476397eebfc76586265c
Israel Jasma <israel.jasma@gmail.com>
2022-04-26 17:28:06 -0400

    Feat: modulos, material, FlexLayout, ngChart, provider DatePie

--- src/app/dashboard-admin/sensors/sensors.module.ts
+++ src/app/dashboard-admin/sensors/sensors.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 import { SensorsRoutingModule } from './sensors-routing.module';
 import { ListComponent } from './pages/list/list.component';
+import { NgChartsModule } from 'ng2-charts';
+import { MaterialModule } from 'src/app/material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LineChartComponent } from './components/line-chart/line-chart.component';
 
 
 @NgModule({
   declarations: [
-    ListComponent
+    ListComponent,
+    LineChartComponent
   ],
   imports: [
     CommonModule,
-    SensorsRoutingModule
+    SensorsRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    NgChartsModule
+  ],
+  providers: [
+    DatePipe
   ]
 })
 export class SensorsModule { }
