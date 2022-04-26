6dba1029bde58cbfd1dc574b462f5e60b95e3a71
Israel Jasma <israel.jasma@gmail.com>
2022-04-26 17:27:08 -0400

    Feat: Componenet line chart

--- src/app/dashboard-admin/sensors/components/line-chart/line-chart.component.ts
+++ src/app/dashboard-admin/sensors/components/line-chart/line-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChartConfiguration, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-line-chart',
@@ -6,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line-chart.component.css']
 })
 export class LineChartComponent implements OnInit {
+
+  @Input() lineChartData!: ChartConfiguration['data']
+
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      line: {
+        tension: 0.5
+      }
+    },
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      x: {},
+      'y-axis-0':
+        {
+          position: 'left',
+        },
+      'y-axis-1': {
+        position: 'right',
+        grid: {
+          color: 'rgba(255,0,0,0.3)',
+        },
+        ticks: {
+          color: 'red'
+        }
+      }
+    }
+  };
+
+  public lineChartType: ChartType = 'line';
 
   constructor() { }
 
