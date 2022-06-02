bc896a14ced8c849e44fa44f19cb621a60269d34
Israel Jasma <israel.jasma@gmail.com>
2022-06-02 14:13:40 -0400

    Feat: modulos material, flexlayout, dashboarduser

--- src/app/dashboard-user/dashboard-user.module.ts
+++ src/app/dashboard-user/dashboard-user.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardUserRoutingModule } from './dashboard-user-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MaterialModule } from '../material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent,
   ],
   imports: [
     CommonModule,
-    DashboardUserRoutingModule
+    DashboardUserRoutingModule,
+    MaterialModule,
+    FlexLayoutModule
   ]
 })
 export class DashboardUserModule { }
