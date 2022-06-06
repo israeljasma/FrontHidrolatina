fde3780e33f44aeb8f64b2ee095294ad5e5792c2
Israel Jasma <israel.jasma@gmail.com>
2022-06-06 16:30:35 -0400

    Feat: declarad detail component

--- src/app/dashboard-user/ppes/ppes.module.ts
+++ src/app/dashboard-user/ppes/ppes.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PpesRoutingModule } from './ppes-routing.module';
+import { DetailComponent } from './pages/detail/detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetailComponent
+  ],
   imports: [
     CommonModule,
     PpesRoutingModule
