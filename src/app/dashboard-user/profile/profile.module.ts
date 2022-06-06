2922d472b6c8d7f9c89dd775a115b9fb4278aa94
Israel Jasma <israel.jasma@gmail.com>
2022-06-05 22:38:03 -0400

    Feat: Profile module

---
+++ src/app/dashboard-user/profile/profile.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProfileRoutingModule } from './profile-routing.module';
+import { DetailComponent } from './detail/detail.component';
+
+
+@NgModule({
+  declarations: [
+    DetailComponent
+  ],
+  imports: [
+    CommonModule,
+    ProfileRoutingModule
+  ]
+})
+export class ProfileModule { }
