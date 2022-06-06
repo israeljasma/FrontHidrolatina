909dcbae9a570ac918af96f18265f25fdc626355
Israel Jasma <israel.jasma@gmail.com>
2022-06-06 16:38:47 -0400

    Feat: Component detail

--- src/app/dashboard-user/actions/actions.module.ts
+++ src/app/dashboard-user/actions/actions.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ActionsRoutingModule } from './actions-routing.module';
+import { DetailComponent } from './pages/detail/detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetailComponent
+  ],
   imports: [
     CommonModule,
     ActionsRoutingModule
