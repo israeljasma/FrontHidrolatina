056823fc0a294da099c5ed2efdc56e106f19217c
Israel Jasma <israel.jasma@gmail.com>
2022-06-05 22:47:20 -0400

    Feat: home component declarad, paths profile, ppes, actions and sensors

--- src/app/dashboard-user/dashboard-user-routing.module.ts
+++ src/app/dashboard-user/dashboard-user-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent
+    component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('./profile/profile.module').then( m => m.ProfileModule )
+      },
+      {
+        path: 'ppes',
+        loadChildren: () => import('./ppes/ppes.module').then( m => m.PpesModule )
+      },
+      {
+        path: 'actions',
+        loadChildren: () => import('./actions/actions.module').then( m => m.ActionsModule )
+      },
+      {
+        path: 'sensors',
+        loadChildren: () => import('./sensors/sensors.module').then( m => m.SensorsModule )
+      }
+    ]
   }
 ];
 
--- src/app/dashboard-user/dashboard-user.module.ts
+++ src/app/dashboard-user/dashboard-user.module.ts
@@ -6,12 +6,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MaterialModule } from '../material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
     NavbarComponent,
+    HomeComponent,
   ],
   imports: [
     CommonModule,
