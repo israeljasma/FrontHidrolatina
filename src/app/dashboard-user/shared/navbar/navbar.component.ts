554f4f762bccf691f723aa80790909a5443c4d41
Israel Jasma <israel.jasma@gmail.com>
2022-06-05 22:30:35 -0400

    Feat: actualizacion componente navbar

--- src/app/dashboard-user/shared/navbar/navbar.component.ts
+++ src/app/dashboard-user/shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  get userAuth(){
+    return this.authService.userAuth;
+  }
+
+  constructor( private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.router.navigate(['./auth']);
+    this.authService.logout();
   }
 
 }
