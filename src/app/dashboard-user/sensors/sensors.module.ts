22468e4f276495701e479ad3b4bd212eeb2931ca
Israel Jasma <israel.jasma@gmail.com>
2022-06-06 17:02:10 -0400

    Feat: Component detail in sensors

--- src/app/dashboard-user/sensors/sensors.module.ts
+++ src/app/dashboard-user/sensors/sensors.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SensorsRoutingModule } from './sensors-routing.module';
+import { DetailComponent } from './pages/detail/detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetailComponent
+  ],
   imports: [
     CommonModule,
     SensorsRoutingModule
