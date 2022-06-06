69091a787107725180c2a7d43f5b67adbfee1ebd
Israel Jasma <israel.jasma@gmail.com>
2022-06-06 17:15:42 -0400

    Feat: Routes actions, ppes, sensors

--- src/app/dashboard-user/actions/actions-routing.module.ts
+++ src/app/dashboard-user/actions/actions-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailComponent } from './pages/detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
